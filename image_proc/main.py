b42ef4823ca942ae97f400213e20e6a0d13a7c3c
dohako <dohako@inbox.ru>
2021-02-18 12:53:40 +0300

    FIX fixed camera photo[3]

--- image_proc/main.py
+++ image_proc/main.py
@@ -5,6 +5,7 @@ import cv2
 _, cam, name, mode = sys.argv
 
 if mode is 'photo':
+    print('Trying take photo')
     cap = cv2.VideoCapture(cam)
     ret,frame = cap.read()
     # cv2.imshow('img1', frame)
