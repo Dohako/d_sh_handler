28c279f9f18899485afd0e0eca02977a4d23ae2c
dohako <dohako@inbox.ru>
2021-04-16 11:12:22 +0300

    CHG autorun expl, audio settings

--- devices/main_audio.py
+++ devices/main_audio.py
@@ -12,17 +12,27 @@ class AudioHandler:
     def __init__(self):
         self.mixers = alsa.mixers()
         if 'Master' in self.mixers:
-            m = alsa.Mixer('Master')  # Headphone
+            self.m = alsa.Mixer('Master')  # Headphone
         elif 'Headphone' in self.mixers:
-            m = alsa.Mixer('Headphone')
+            self.m = alsa.Mixer('Headphone')
         else:
-            m = None
-        if m:
-            current_volume = m.getvolume()
-            m.setvolume(0)
+            self.m = None
+        if self.m:
+            current_volume = self.m.getvolume()
+            self.m.setvolume(0)
 
     @staticmethod
     def change_volume(volume: str):
+        int_volume = AudioHandler.normalize_param(volume)
+        audio_answer = subprocess.call(['amixer', '-D', 'pulse', 'sset', 'Master', f'{int_volume}%'])
+        return audio_answer
+
+    def change_volume_alsa(self,volume:str):
+        int_volume = AudioHandler.normalize_param(volume)
+        self.m.setvolume(int_volume)
+
+    @staticmethod
+    def normalize_param(volume):
         if volume.isdigit():
             int_volume = int(volume)
             if int_volume > 150:
@@ -31,15 +41,15 @@ class AudioHandler:
                 int_volume = 0
         else:
             int_volume = 0
-        audio_answer = subprocess.call(['amixer', '-D', 'pulse', 'sset', 'Master', f'{int_volume}%'])
-        return audio_answer
+        return int_volume
 
     @staticmethod
     def test_mixers():
         mixers = alsa.mixers()
         answer = []
         for mixer in mixers:
-            answer.append(subprocess.call(['amixer', '-D', 'pulse', 'sset', f'{mixer}', f'0%']))
+            sub_return = subprocess.call(['amixer', '-D', 'pulse', 'sset', f'{mixer}', f'0%'])
+            answer.append([mixer,sub_return])
         return answer
 
 
@@ -48,4 +58,4 @@ if __name__ == '__main__':
     if os.name != 'nt':
         if mode == 'test':
             print(AudioHandler.test_mixers())
-        # AudioHandler.change_volume('100')
+        AudioHandler.change_volume('100')
