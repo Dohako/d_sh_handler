f96456f6024902e73505be2f1180d39bc4bd1856
dohako <dohako@inbox.ru>
2021-04-24 15:12:47 +0300

    REF main_t_bot #

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 from loguru import logger
-from os import name, system, mkdir
-from os.path import abspath, dirname, isdir
+from os import system, mkdir
+from os.path import dirname, isdir
 from time import sleep, time
 from multiprocessing import Process
 from psutil import virtual_memory, cpu_percent
 from datetime import datetime
 
 from t_bot.main_t_bot import MainBot
+
+
+def run_bot():
+    """
+    Method for chat_bot starting
+    """
+    logger.info('-------------------------')
+    logger.info('trying to start bot')
+    # subprocess.run(['python3', bot_dir])
+    MainBot()
+    logger.info('-------------------------')
+
+
+def run_voice_rec():
+    """
+    Method for voice recognition starting
+    """
+    logger.info('-------------------------')
+    logger.info('trying to start voice_rec')
+    # subprocess.run(['python3', voice_rec_dir])
+    sleep(10)
+    logger.info('-------------------------')
 
 
 class MainClass:
@@ -16,26 +38,6 @@ class MainClass:
     In project we have two interfaces to interact with system: chat bot
     and voice recognition system
     """
-
-    def run_bot(self):
-        """
-        Method for chat_bot starting
-        """
-        logger.info('-------------------------')
-        logger.info('trying to start bot')
-        # subprocess.run(['python3', bot_dir])
-        MainBot()
-        logger.info('-------------------------')
-
-    def run_voice_rec(self):
-        """
-        Method for voice recognition starting
-        """
-        logger.info('-------------------------')
-        logger.info('trying to start voice_rec')
-        # subprocess.run(['python3', voice_rec_dir])
-        sleep(10)
-        logger.info('-------------------------')
 
     def new_ver_checker(self):
         pass
@@ -71,23 +73,23 @@ class MainClass:
     @logger.catch()
     def start(self):
         logger.info("trying to start scripts")
-        chat_bot_process = Process(target=self.run_bot)
-        voice_recognition_process = Process(target=self.run_voice_rec)
+        chat_bot_process = Process(target=run_bot)
+        voice_recognition_process = Process(target=run_voice_rec)
         # check current versions for all components
         self.take_versions()
-        kill_voice_rec = False
-        kill_bot_proc = False
+        # kill_voice_rec = False
+        # kill_bot_proc = False
         # timers
         time_start = int(time())
         checking_time = time_start
-        checking_new_ver_time = time_start
+        # checking_new_ver_time = time_start
         # checker_process = multiprocessing.Process(target=new_ver_checker)
         # main loop
         while True:
             sleep(3)
             if chat_bot_process.is_alive() is False:
                 logger.info("Started chat_bot")
-                chat_bot_process = Process(target=self.run_bot)
+                chat_bot_process = Process(target=run_bot)
                 chat_bot_process.start()
                 sleep(1)
             # TODO restore voice_rec
@@ -145,28 +147,28 @@ class MainClass:
                 pass
 
     # This two methods should do what?...
-    def set_linux_dirs(self):
-        # TODO change to dynamic
-        # main loop scripts dirs
-        bot_dir = f"/home/pi/d_sh_handler/t_bot/main.py"
-        bot_ver_dir = f"/home/pi/d_sh_handler/t_bot/ver.txt"
-        voice_rec_dir = f"/home/pi/d_sh_handler/voice_rec/main.py"
-        voice_rec_ver_dir = f"/home/pi/d_sh_handler/voice_rec/ver.txt"
-        # other scripts
-        image_proc_dir = f"/home/pi/d_sh_handler/image_proc/main.py"
-        image_proc_ver_dir = f"/home/pi/d_sh_handler/image_proc/ver.txt"
-        devices_ver_dir = f"/home/pi/d_sh_handler/devices/ver.txt"
-
-    def set_windows_dirs(self):
-        bot_dir = f"{dirname(abspath('.'))}\\t_bot\\main.py"
-        bot_ver_dir = f"{dirname(abspath('.'))}\\t_bot\\ver.txt"
-        voice_rec_dir = f"{dirname(abspath('.'))}\\voice_rec\\main.py"
-        voice_rec_ver_dir = f"{dirname(abspath('.'))}\\voice_rec\\ver.txt"
-        # other scripts
-        image_proc_dir = f"{dirname(abspath('.'))}\\image_proc\\main.py"
-        image_proc_ver_dir = f"{dirname(abspath('.'))}\\image_proc\\ver.txt"
-
-        devices_ver_dir = f"{dirname(abspath('.'))}\\devices\\ver.txt"
+    # def set_linux_dirs(self):
+    #     # TODO change to dynamic
+    #     # main loop scripts dirs
+    #     bot_dir = f"/home/pi/d_sh_handler/t_bot/main.py"
+    #     bot_ver_dir = f"/home/pi/d_sh_handler/t_bot/ver.txt"
+    #     voice_rec_dir = f"/home/pi/d_sh_handler/voice_rec/main.py"
+    #     voice_rec_ver_dir = f"/home/pi/d_sh_handler/voice_rec/ver.txt"
+    #     # other scripts
+    #     image_proc_dir = f"/home/pi/d_sh_handler/image_proc/main.py"
+    #     image_proc_ver_dir = f"/home/pi/d_sh_handler/image_proc/ver.txt"
+    #     devices_ver_dir = f"/home/pi/d_sh_handler/devices/ver.txt"
+    #
+    # def set_windows_dirs(self):
+    #     bot_dir = f"{dirname(abspath('.'))}\\t_bot\\main.py"
+    #     bot_ver_dir = f"{dirname(abspath('.'))}\\t_bot\\ver.txt"
+    #     voice_rec_dir = f"{dirname(abspath('.'))}\\voice_rec\\main.py"
+    #     voice_rec_ver_dir = f"{dirname(abspath('.'))}\\voice_rec\\ver.txt"
+    #     # other scripts
+    #     image_proc_dir = f"{dirname(abspath('.'))}\\image_proc\\main.py"
+    #     image_proc_ver_dir = f"{dirname(abspath('.'))}\\image_proc\\ver.txt"
+    #
+    #     devices_ver_dir = f"{dirname(abspath('.'))}\\devices\\ver.txt"
 
 
 if __name__ == '__main__':
--- t_bot/main_t_bot.py
+++ t_bot/main_t_bot.py
@@ -1,52 +1,51 @@
-import sys
-import os
-if os.name != 'nt':
-    import alsaaudio
-    # TODO change to dynamic
-    sys.path.append(f"/home/pi/d_sh_handler")
-import subprocess
-import time
-from t_bot import bot_handler
+from sys import path
+from os import name as os_name, getenv, mkdir
+from os.path import isdir, dirname, abspath, exists
+from subprocess import call
+from time import sleep, time
 from dotenv import load_dotenv
-import datetime
-import loguru
+from datetime import datetime
+from loguru import logger
+from multiprocessing import Process
+from glob import glob
+
+# if os_name != 'nt':
+#     # TODO change to dynamic
+#     path.append(f"/home/pi/d_sh_handler")
+
+from t_bot.bot_handler import BotHandler
+# from t_bot import bot_handler
 from utils.courses import scrap_currency_from_page, get_pycbrf_course
 from utils.take_photo import photo
 from devices import video
-import multiprocessing
-import glob
 
 
 class MainBot:
 
     def __init__(self):
-        # if os.name != 'nt':
-        #     loguru.logger.add('/home/pi/d_sh_handler/log.log')
-        # else:
-        #     loguru.logger.add('log.log')
-        loguru.logger.info("Started bot")
+        logger.info("Started bot")
         load_dotenv()
-        token = os.getenv('MY_TOKEN')
+        token = getenv('MY_TOKEN')
 
-        if os.name != 'nt':
+        if os_name != 'nt':
             # image_proc_dir = f"{os.path.dirname(os.path.abspath('.'))}/image_proc/main.py"
             # photo_dir = f"{os.path.dirname(os.path.abspath('.'))}/photos"
             self.image_proc_dir = f"/home/pi/d_sh_handler/image_proc/main.py"
             self.photo_dir = f"/home/pi/d_sh_handler/photos"
-            if os.path.isdir(self.photo_dir) is False:
-                os.mkdir(self.photo_dir)
+            if isdir(self.photo_dir) is False:
+                mkdir(self.photo_dir)
             self.video_dir = f"/home/pi/d_sh_handler/videos"
-            if os.path.isdir(self.video_dir) is False:
-                os.mkdir(self.video_dir)
+            if isdir(self.video_dir) is False:
+                mkdir(self.video_dir)
         else:
-            self.image_proc_dir = f"{os.path.dirname(os.path.abspath('.'))}\\image_proc\\main.py"
+            self.image_proc_dir = f"{dirname(abspath('.'))}\\image_proc\\main.py"
 
-        self.bot = bot_handler.BotHandler(token)
+        self.bot = BotHandler(token)
         self.greetings_list = ('hello', '/hi', 'qq', 'greetings')
         self.currency_list = ("eur", "usd")
         self.currency_list_correct = ("EUR", "USD")
         self.bot_key = '/'
-        self.now = datetime.datetime.now()
+        self.now = datetime.now()
         self.admin_id = '388863805'
         self.valid_chats = [388863805, -259505319, -342305508]
         self.admin_commands_list = ['/add_chat_id', '/show_active_chats']
@@ -54,7 +53,7 @@ class MainBot:
         self.photo_commands = ['p', 'photo', 'take_photo', 'фото', 'сфотографируй']
         self.video_commands = ['video', 'start_video']
         self.light_commands = ['l']
-        self.state_commands = ['s','state']
+        self.state_commands = ['s', 'state']
         self.show_last_logs_commands = ['show_last_log']
         self.admin_reboot_device_commands = ['reboot']
         self.renew_ver_git_commands = ['git_renew']
@@ -70,14 +69,14 @@ class MainBot:
                 self.main()
             except KeyboardInterrupt:
                 quit()
-            except:
-                loguru.logger.info("Exception occurred, waiting 15 secs and rebooting script")
-                time.sleep(15)
+            except Exception:
+                logger.info("Exception occurred, waiting 15 secs and rebooting script")
+                sleep(15)
 
     def send_message(self, last_chat_id):
         self.bot.send_message(last_chat_id, f"_")
 
-    def send_greetings(self,chat,name, time_now):
+    def send_greetings(self, chat, name, time_now):
         if 6 <= time_now < 12:
             self.bot.send_message(chat, f"Доброе утро, {name}")
         elif 12 <= time_now < 18:
@@ -87,25 +86,8 @@ class MainBot:
 
     def main(self):
         new_offset = None
-        today = self.now.day
+        # today = self.now.day
         hour = self.now.hour
-
-        if os.name != 'nt':
-            mixers = alsaaudio.mixers()
-            if 'Master' in mixers:
-                m = alsaaudio.Mixer('Master')  # Headphone
-            elif 'Headphone' in mixers:
-                m = alsaaudio.Mixer('Headphone')
-            else:
-                loguru.logger.info(mixers)
-                loguru.logger.info('cant find mixer, help')
-                m = None
-                # quit()
-            # current_volume = m.getvolume()
-            # m.setvolume(0)
-            # loguru.logger.debug(f'current volume is set from {current_volume} to {0}')
-        else:
-            m = None
 
         while True:
             self.bot.get_updates(new_offset)
@@ -115,7 +97,7 @@ class MainBot:
             if not last_update:
                 continue
 
-            loguru.logger.debug(last_update)
+            logger.debug(last_update)
             last_update_id = last_update['update_id']
 
             if 'date' not in last_update['message'].keys():
@@ -123,7 +105,7 @@ class MainBot:
                 continue
             else:
                 time_of_update = last_update['message']['date']
-            if int(time.time()) > time_of_update + 5:
+            if int(time()) > time_of_update + 5:
                 new_offset = last_update_id + 1
                 continue
 
@@ -140,8 +122,9 @@ class MainBot:
             last_chat_id = last_update['message']['chat']['id']
             last_message_sender_name = last_update['message']['from']['first_name']
             if last_chat_id not in self.valid_chats:
-                message_to_send = f'unregistered attempt with {last_message_sender_name}, {last_chat_id}, {last_chat_text}'
-                loguru.logger.info(message_to_send)
+                message_to_send = f'unregistered attempt with {last_message_sender_name}, {last_chat_id}, ' \
+                                  f'{last_chat_text}'
+                logger.info(message_to_send)
                 self.bot.send_message(self.admin_id, message_to_send)
                 new_offset = last_update_id + 1
                 continue
@@ -158,11 +141,11 @@ class MainBot:
                 cmd = message.split(self.bot_key)[1].split(' ')[0]
                 if len(message.split(self.bot_key)[1].split(' ')) > 1:
                     param = message.split(self.bot_key)[1].split(' ')[1]
-                    loguru.logger.debug(param)
+                    logger.debug(param)
                 else:
                     param = None
                 if cmd in self.greetings_list:
-                    self.send_greetings(last_chat_id,last_message_sender_name,hour)
+                    self.send_greetings(last_chat_id, last_message_sender_name, hour)
 
                 elif cmd in self.renew_ver_git_commands:
                     pass
@@ -172,7 +155,7 @@ class MainBot:
                     currency_name = self.currency_list_correct[self.currency_list.index(currency)]
                     # pycbrf_todays_currency = rates[currency_name].value
                     pycbrf_todays_currency = get_pycbrf_course(currency_name)
-                    loguru.logger.debug(currency_name)
+                    logger.debug(currency_name)
                     moex_answer = scrap_currency_from_page(currency_name)
                     self.bot.send_message(last_chat_id,
                                           f"1 {currency_name} = {pycbrf_todays_currency} RUB CBRF, "
@@ -185,30 +168,26 @@ class MainBot:
                 #         greet_bot.send_message(last_chat_id, f"1 {currency_name} = {rates[currency_name].value} RUB")
 
                 elif cmd in self.volume_commands:
-                    if os.name != 'nt':
-                        loguru.logger.debug("Управление звуком зарегистрировано")
+                    if os_name != 'nt':
+                        logger.debug("Управление звуком зарегистрировано")
                         volume = param
                         if volume.isdigit():
                             # TODO change way to control sound level
                             int_volume = int(volume)
                             if int_volume > 150:
-                                # m.setvolume(150)
-                                audio_answer = subprocess.call(['amixer', '-D', 'pulse', 'sset', 'Master', '100%'])
+                                audio_answer = call(['amixer', '-D', 'pulse', 'sset', 'Master', '100%'])
                                 self.bot.send_message(last_chat_id,
                                                       f"Ставлю звук на максимум({audio_answer})")
                             elif int_volume < 0:
-                                # m.setvolume(0)
-                                audio_answer = subprocess.call(['amixer', '-D', 'pulse', 'sset', 'Master', '0%'])
+                                audio_answer = call(['amixer', '-D', 'pulse', 'sset', 'Master', '0%'])
                                 self.bot.send_message(last_chat_id,
                                                       f"Выключаю звук({audio_answer})")
                             else:
-                                # m.setvolume(int_volume)
-                                audio_answer = subprocess.call(['amixer', '-D', 'pulse', 'sset', 'Master', f'{int_volume}%'])
+                                audio_answer = call(['amixer', '-D', 'pulse', 'sset', 'Master', f'{int_volume}%'])
                                 self.bot.send_message(last_chat_id,
                                                       f"Ставлю звук на {int_volume}({audio_answer})")
                         else:
-                            # m.setvolume(0)
-                            audio_answer = subprocess.call(['amixer', '-D', 'pulse', 'sset', 'Master', '0%'])
+                            audio_answer = call(['amixer', '-D', 'pulse', 'sset', 'Master', '0%'])
                             self.bot.send_message(last_chat_id,
                                                   f"Команда не распознана до конца, выключаю звук({audio_answer})")
 
@@ -216,8 +195,7 @@ class MainBot:
                         self.bot.send_message(last_chat_id, f"Не та ОС")
 
                 elif cmd in self.photo_commands:
-                    photo_name = f'{self.photo_dir}/{datetime.datetime.now().strftime("%d%m%Y-%H%M")}.png'
-                    # subprocess.call(f'fswebcam -q -r 1280x720 {photo_name}', shell=True)
+                    photo_name = f'{self.photo_dir}/{datetime.now().strftime("%d%m%Y-%H%M")}.png'
                     if param is None:
                         cam = 0
                     else:
@@ -227,7 +205,7 @@ class MainBot:
                             cam = 0
                     # TODO make class and refactor
                     photo(photo_name, cam)
-                    if os.path.exists(photo_name):
+                    if exists(photo_name):
                         self.bot.send_photo(last_chat_id, photo_name)
                     else:
                         self.bot.send_message(last_chat_id, f"Ошибка с формированием и отправкой фото")
@@ -238,7 +216,7 @@ class MainBot:
                         new_offset = last_update_id + 1
                         continue
                     self.bot.send_message(last_chat_id, f"Начинаю запись видео")
-                    self.my_stamp_for_video = datetime.datetime.now().strftime("%d%m%Y-%H%M")
+                    self.my_stamp_for_video = datetime.now().strftime("%d%m%Y-%H%M")
                     self.video_name = f'{self.video_dir}/{self.my_stamp_for_video}'
                     if param is None:
                         self.record_qty = 1
@@ -248,7 +226,7 @@ class MainBot:
                         else:
                             self.record_qty = 1
                     try:
-                        self.video_processing = multiprocessing.Process(target=self.video_thread)
+                        self.video_processing = Process(target=self.video_thread)
                     except Exception as ex:
                         self.bot.send_message(last_chat_id, f"{ex}")
                         new_offset = last_update_id + 1
@@ -261,13 +239,13 @@ class MainBot:
                     self.bot.send_message(last_chat_id, f"Это еще не реализовано")
             if self.video_trigger:
                 if self.video_processing.is_alive() is False:
-                    list_of_videos = glob.glob(os.path.abspath(f'{self.video_dir}/*'))
+                    list_of_videos = glob(abspath(f'{self.video_dir}/*'))
                     for video_name in list_of_videos:
                         if video_name.split("/")[-1].split('_')[0] == self.my_stamp_for_video:
                             self.bot.send_video(self.chat_to_send_video, video_name)
                     self.video_trigger = False
             new_offset = last_update_id + 1
-            loguru.logger.debug(new_offset)
+            logger.debug(new_offset)
 
     def video_thread(self):
         v = video.VideoHandler(record_qty=self.record_qty,
