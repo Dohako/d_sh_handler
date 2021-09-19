7b0bde9a6e2e060569ae00152428182129825d6a
dohako <dohako@inbox.ru>
2021-09-19 19:33:26 +0300

    testing cicd

--- rpi_cicd/git_handler.py
+++ rpi_cicd/git_handler.py
@@ -2,15 +2,23 @@
 This script will check git updates every N time and if there is some, then it will pull it and quit
 """
 
-from os import name
+from genericpath import isdir
+from os import name, isdir, mkdir
+from loguru import logger
+from datetime import datetime
+from os.path import dirname
 import subprocess
 
 def main():
+    path = dirname(__file__)
+    if isdir(f'{path}/logs') is False:
+        mkdir(f'{path}/logs')
+    logger.add(f'{path}/logs/log{datetime.now().strftime("%d%m%Y_%H%M")}.log')
     try:
         subprocess.run("/home/pi/d_sh_handler/rpi_cicd/git-repo-watcher -d /home/pi/d_sh_handler".split(), check=True, text=True)
     except Exception as ex:
-        print("*"*100)
-        print(ex)
+        logger.info("*"*100)
+        logger.info(ex)
         if "10" in ex:
             return "repo is updated"
         raise
