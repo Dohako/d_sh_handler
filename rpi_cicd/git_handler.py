e006193a6810de7328c90f69c5ecb56d75f7eb12
dohako <dohako@inbox.ru>
2021-09-19 17:34:09 +0300

    testing cicd

--- rpi_cicd/git_handler.py
+++ rpi_cicd/git_handler.py
@@ -3,9 +3,10 @@ This script will check git updates every N time and if there is some, then it wi
 """
 
 import subprocess
-
-results = subprocess.run("/home/pi/d_sh_handler/rpi_cicd/git-repo-watcher -d /home/pi/d_sh_handler".split(), check=True, text=True)
-
+try:
+    results = subprocess.run("/home/pi/d_sh_handler/rpi_cicd/git-repo-watcher -d /home/pi/d_sh_handler".split(), check=True, text=True)
+except Exception as ex:
+    print(ex)
 print(results.stdout)
 
 # subprocess.run("dir", 
