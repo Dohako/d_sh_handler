21dc7fc27fbe38b81c8db3ad05ced536898bbc20
dohako <dohako@inbox.ru>
2021-09-19 17:11:08 +0300

    testing cicd

--- rpi_cicd/git_handler.py
+++ rpi_cicd/git_handler.py
@@ -7,5 +7,6 @@ import subprocess
 results = subprocess.run("/home/pi/d_sh_handler/rpi_cicd/git-repo-watcher -d /home/pi/d_sh_handler".split(), check=True, text=True)
 
 print(results.stdout)
+
 # subprocess.run("dir", 
 # check=True, text=True, shell=True)
