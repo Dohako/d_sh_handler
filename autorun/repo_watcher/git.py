b75582cc243ecacc6837d11c2de7d60729bf89b6
dohako <dohako@inbox.ru>
2021-09-19 16:12:22 +0300

    testing git bash

---
+++ autorun/repo_watcher/git.py
@@ -0,0 +1,10 @@
+"""
+This script will check git updates every N time and if there is some, then it will pull it and quit
+"""
+
+import subprocess
+
+subprocess.run("/home/pi/d_sh_handler/autorun/repo_watcher/git-repo-watcher -d /home/pi/d_sh_handler", 
+check=True, text=True, shell=True)
+# subprocess.run("dir", 
+# check=True, text=True, shell=True)
