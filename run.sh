557a4aac4d5adbb19d714b53ce34e339584a6ab5
Denis Pankratev <d.pankratev@raicoon.com>
2024-09-23 22:52:18 +0200

    add part to kill existing process to update

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 VENV_DIR="/home/denis/d_sh_handler/venv"
 
+if pgrep -f "python.*main.py" > /dev/null; then
+    echo "Found running process of main.py. Killing it..."
+    pkill -f "python.*main.py"
+else
+    echo "No running process of main.py found."
+fi
+
 if [ ! -d "$VENV_DIR" ]; then
     cd /home/denis/d_sh_handler
     python -m venv venv
