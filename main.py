71e15c4c2643edf761f77d402c46c5b7eaf71dcb
Denis Pankratev <d.pankratev@raicoon.com>
2024-09-25 22:32:48 +0200

    set a proper starting message

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import time
 import requests
@@ -5,11 +6,11 @@ import requests
 # Replace these with your bot's API token and your chat ID
 BOT_API_TOKEN = os.getenv('BOT_API_TOKEN')
 TG_ADMIN_ID = os.getenv('TG_ADMIN_ID')
-if not BOT_API_TOKEN:
+if not BOT_API_TOKEN or not TG_ADMIN_ID:
     raise Exception("no env vars")
 MESSAGE = 'Hello, Denis! This is a test message from your bot.'
 
-def send_telegram_message(bot_token, chat_id, message):
+def send_raw_telegram_message(bot_token, chat_id, message) -> None:
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     data = {
         'chat_id': chat_id,
@@ -22,7 +23,7 @@ def send_telegram_message(bot_token, chat_id, message):
     else:
         print(f"Failed to send message. Status code: {response.status_code}, Response: {response.text}")
 
-def get_updates(bot_token):
+def get_raw_updates(bot_token):
     url = f"https://api.telegram.org/bot{bot_token}/getUpdates"
     response = requests.get(url)
     if response.status_code == 200:
@@ -33,7 +34,11 @@ def get_updates(bot_token):
 
 
 if __name__ == "__main__":
-    get_updates(BOT_API_TOKEN)
-    send_telegram_message(BOT_API_TOKEN, TG_ADMIN_ID, "hoba1111")
+    get_raw_updates(BOT_API_TOKEN)
+    send_raw_telegram_message(
+        BOT_API_TOKEN, 
+        TG_ADMIN_ID, 
+        f"Hello! Main program has started started, servers time is: {datetime.now().isoformat()}"
+    )
     time.sleep(100)
 
